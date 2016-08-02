bcafbb6d478936dd0f75a1ee4935cc66dbaa9aff
Sam Li <samli@google.com>
2016-08-02 16:54:02 +1000

    Fast inline demos and updateable

--- src/user_content.py
+++ src/user_content.py
@@ -1,7 +1,7 @@
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 
-from dataModel import Library, Version, Content
+from datamodel import Library, Version, Content
 
 import webapp2
 import json
@@ -64,6 +64,7 @@ class GetResource(webapp2.RequestHandler):
     else:
       self.response.set_status(301)
       self.response.headers['Location'] = str(resolved)
+      self.response.headers['cache-control'] = 'max-age=315569000'
 
 # pylint: disable=invalid-name
 app = webapp2.WSGIApplication([
