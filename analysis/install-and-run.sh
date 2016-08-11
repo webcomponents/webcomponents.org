d9f9b5232ed5d623ffb1d178f0ab408b5c35754e
Andy Mutton <andymutton@google.com>
2016-08-11 13:35:45 +1000

    updated

--- analysis/install-and-run.sh
+++ analysis/install-and-run.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-# We're in a GCE instance if we're running under a google service account.
+
+##
+# Startup script for analysis servers.
+# Installs dependencies and launchs analysis if running in a GCE instance.
+##
+
+# Determine whether we're a GCE instance. We need this for later decisions.
 IN_GCE_INSTANCE=$(gcloud info | grep gserviceaccount | cut -d "[" -f2 | cut -d "]" -f1 | wc -l)
 
 if [[ $IN_GCE_INSTANCE -ge 1 ]] ; then
-  # make a place that the code can be uploaded to.
+  # Create directory for code to be deployed to by deploy.sh, then cd to it.
   mkdir -m 777 /usr/local/lib/analysis
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | NVM_DIR=/usr/local/lib/nvm bash
-  NVM_SH=/usr/local/lib/nvm/nvm.sh
-else
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-  NVM_SH=~/.nvm/nvm.sh
+  cd /usr/local/lib/analysis
+
+  # Install Stackdriver monitoring for instance data
+  curl -O "https://repo.stackdriver.com/stack-install.sh"
+  sudo bash stack-install.sh --write-gcm
 fi
 
-# Install dependencies for analysis - if they can't be installed, exit.
-sudo apt-get update && sudo apt-get install -y make git || exit
+# Install nvm to maintain correct node/npm versions
+[[ $IN_GCE_INSTANCE -ge 1 ]] && NVM_DIR=/usr/local/lib/nvm || NVM_DIR=~/.nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | NVM_DIR=$NVM_DIR bash
 
-. $NVM_SH && \
+# Install make and git for analysis - if they can't be installed, reboot and try again.
+sudo apt-get update && sudo apt-get install -y make git || (sleep 60 && sudo reboot)
+
+# Install the correct version of node.js and the dependencies we need.
+. $NVM_DIR/nvm.sh && \
 nvm install v4.4.5 && \
-npm install bower hydrolysis gcloud repeat
+npm install || (sleep 10 && sudo reboot)
 
-# Start analysis if we're running in a GCE.
-if [[ $IN_GCE_INSTANCE -ge 1 ]]
-then
+# Automatically start analysis if we're running in a GCE.
+if [[ $IN_GCE_INSTANCE -ge 1 ]] ; then
   GCLOUD_PROJECT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/project -H "Metadata-Flavor: Google")
   TOPIC=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/topic -H "Metadata-Flavor: Google")
   node /usr/local/lib/analysis/main.js $GCLOUD_PROJECT $TOPIC &
