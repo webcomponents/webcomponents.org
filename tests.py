c82fd023912ab59d684ad33703c1033b5fce7d8d
Sam Li <samli@google.com>
2016-08-15 13:56:00 +1000

    print sys.path

--- tests.py
+++ tests.py
@@ -28,6 +28,7 @@ def main(sdk_path):
   # changes to configuration there are available to all tests (e.g.
   # sys.path modifications, namespaces, etc.)
   # try:
+  print sys.path
   import google
   print "test.py", google.__path__
   import appengine_config
