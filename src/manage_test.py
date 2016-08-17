66e498d6c9d1ffa93ff4b03fd892bfdac2f96963
Sam Li <samli@google.com>
2016-08-17 15:32:32 +1000

    Don't load all stubs

--- src/manage_test.py
+++ src/manage_test.py
@@ -17,7 +17,10 @@ class ManageTestBase(unittest.TestCase):
   def setUp(self):
     self.testbed = testbed.Testbed()
     self.testbed.activate()
-    self.testbed.init_all_stubs()
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_urlfetch_stub()
+    self.testbed.init_memcache_stub()
+    self.testbed.init_taskqueue_stub()
 
     self._expected_fetches = []
     # pylint: disable=protected-access
