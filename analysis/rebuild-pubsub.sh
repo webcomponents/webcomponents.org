a6a497edca96cffcf97681b28c452f9c71278b25
Andy Mutton <andymutton@google.com>
2016-08-29 12:08:11 +1000

    Updated pubsub init script

--- analysis/rebuild-pubsub.sh
+++ analysis/rebuild-pubsub.sh
@@ -2,22 +2,27 @@
 
 function rebuild_pubsub {
   echo "Deleting request subscription..."
-  gcloud alpha pubsub subscriptions delete analysis-requests
+  gcloud --project $1 alpha pubsub subscriptions delete analysis-requests
 
   echo "Deleting response subscription..."
-  gcloud alpha pubsub subscriptions delete analysis-responses
+  gcloud --project $1 alpha pubsub subscriptions delete analysis-responses
 
   echo "Deleting request topic..."
-  gcloud alpha pubsub topics delete analysis-requests
+  gcloud --project $1 alpha pubsub topics delete analysis-requests
   echo "Deleting response topic..."
-  gcloud alpha pubsub topics delete analysis-responses
+  gcloud --project $1 alpha pubsub topics delete analysis-responses
 
   echo "Creating response topic and push subscription..."
-  gcloud alpha pubsub topics create analysis-requests
-  gcloud alpha pubsub topics create analysis-responses
-  gcloud alpha pubsub subscriptions create analysis-responses --topic analysis-responses --ack-deadline 60 --push-endpoint https://manage-dot-custom-elements.appspot.com/_ah/push-handlers/analysis
-  gcloud alpha pubsub subscriptions create analysis-requests --topic analysis-requests --ack-deadline 60 --push-endpoint https://analysis-dot-custom-elements.appspot.com/process/next
+  gcloud --project $1 alpha pubsub topics create analysis-requests
+  gcloud --project $1 alpha pubsub topics create analysis-responses
+  gcloud --project $1 alpha pubsub subscriptions create analysis-responses --topic analysis-responses --ack-deadline 60 --push-endpoint https://manage-dot-$1.appspot.com/_ah/push-handlers/analysis
+  gcloud --project $1 alpha pubsub subscriptions create analysis-requests --topic analysis-requests --ack-deadline 60 --push-endpoint https://analysis-dot-$1.appspot.com/process/next
 
 }
 
-rebuild_pubsub
+if (( $# != 1 ))
+then
+  echo "Usage: rebuild_pubsub [custom-elements, custom-elements-staging]"
+  exit 1
+fi
+rebuild_pubsub $1
