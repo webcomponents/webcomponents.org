1cee8de3afaa7609ae16c6f5c0ff30db630d1fa2
Douglas Stockwell <dstockwell@chromium.org>
2016-08-30 10:26:04 +1000

    Add error description when dependencies cant be cound

--- src/user_content.py
+++ src/user_content.py
@@ -22,6 +22,7 @@ class GetResource(webapp2.RequestHandler):
 
     dependencies = json.loads(analysis.content).get('bowerDependencies', None)
     if dependencies is None:
+      self.response.write('could not find dependencies for %s in %s/%s' % (tag, owner, repo))
       self.response.set_status(404)
       return
 
