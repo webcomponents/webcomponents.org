89a76b47a42d1069107a88d8c918506da7b04e75
Andy Mutton <andymutton@google.com>
2016-09-19 13:21:38 +1000

    Fix tabs and make easier to read

--- analysis/test/bower-test.js
+++ analysis/test/bower-test.js
@@ -3,18 +3,15 @@ var expect = require('chai').expect;
 
 var Bower = require('../src/bower');
 
-describe("Bower", function() {
-	describe("Multiple installs", function() {
-		it("returns the same data", function() {
-      this.timeout(15000);
-      var bower = new Bower();
-      return bower.install("PolymerElements", "paper-radio-button", "v1.2.1")
-          .then(function(data) {
-            bower.install("PolymerElements", "paper-radio-button", "v1.2.1")
-              .then(function(secondData) {
-                expect(data).to.deep.equal(secondData);
-              });
-          });
+describe("Bower - multiple installs", function() {
+  it("returns the same data", function() {
+    this.timeout(15000);
+    var bower = new Bower();
+    return bower.install("PolymerElements", "paper-radio-button", "v1.2.1").then(function(first) {
+      bower.install("PolymerElements", "paper-radio-button", "v1.2.1").then(function(second) {
+        expect(second).to.be.not.empty();
+        expect(first).to.deep.equal(second);
+      });
     });
-	});
+  });
 });
