c5423159cb777f9094ef8d96e83d5c9d8ad180f4
Andy Mutton <andymutton@google.com>
2016-09-20 17:27:47 +1000

    Add additional path filtering to remove things that shouldn't be there.

--- analysis/src/hydrolysis.js
+++ analysis/src/hydrolysis.js
@@ -2,6 +2,7 @@
 
 const Ana = require('./ana_log');
 const hyd = require('hydrolysis');
+const path = require('path');
 
 /**
  * Service for running Hydrolysis on the local machine.
@@ -29,10 +30,21 @@ class Hydrolysis {
         mainHtmlPaths.map(mainHtmlPath => {
           return hyd.Analyzer.analyze(mainHtmlPath, {clean: true, filter: () => false})
             .then(analyzer => {
+
+              // There's a weird name.indexOf === 0 thing in hydrolysis that means that
+              // paper-dialog-x will be included in paper-dialog. We don't want that, so
+              // we need to add this additional filtering.
+              var pathFilter = item => {
+                return !path.relative(item.contentHref, mainHtmlPath).includes("..");
+              };
+
               // Get only the elements in the folder containing the element we're looking at.
-              var elements = analyzer.elementsForFolder(mainHtmlPath);
-              var behaviors = analyzer.behaviorsForFolder(mainHtmlPath);
+              var elements = analyzer.elementsForFolder(mainHtmlPath).filter(pathFilter);
+              var behaviors = analyzer.behaviorsForFolder(mainHtmlPath).filter(pathFilter);
+
               Ana.debug("Got elements and behaviors");
+              // Ana.debug("Elements", elements);
+              Ana.debug("Behaviors", behaviors);
 
               // Strip useless (bloated) parts from the elements.
               elements.forEach(element => {
