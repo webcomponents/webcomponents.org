3d8066ee19675b92b8a671819842f9c36531bd6b
Andy Mutton <andymutton@google.com>
2016-09-30 17:31:23 +1000

    Add log output to non-transient failure responses (#320)

--- analysis/src/ana_log.js
+++ analysis/src/ana_log.js
@@ -1,6 +1,22 @@
 'use strict';
 
+var Console = require('console').Console;
+var streamBuffers = require('stream-buffers');
+
 var debug = false;
+
+var logBuffer;
+var bufferedConsole;
+
+function initConsole() {
+  logBuffer = new streamBuffers.WritableStreamBuffer({
+      initialSize: (100 * 1024),
+      incrementAmount: (10 * 1024)
+  });
+  bufferedConsole = new Console(logBuffer, logBuffer);
+}
+
+initConsole();
 
 class Ana {
   static success(counterPath) {
@@ -8,8 +24,10 @@ class Ana {
     if (arguments.length > 1) {
       var logArgs = [path].concat(Array.prototype.slice.call(arguments, 1));
       console.log.apply(null, logArgs);
+      bufferedConsole.log.apply(null, logArgs);
     } else {
       console.log(path);
+      bufferedConsole.log(path);
     }
   }
   static fail(counterPath) {
@@ -17,23 +35,33 @@ class Ana {
     if (arguments.length > 1) {
       var logArgs = [path].concat(Array.prototype.slice.call(arguments, 1));
       console.log.apply(null, logArgs);
+      bufferedConsole.log.apply(null, logArgs);
     } else {
       console.log(path);
+      bufferedConsole.log(path);
     }
   }
   static debug() {
     if (debug) {
+      // Don't buffer debug logs
       console.log.apply(null, arguments);
     }
   }
   static log() {
     console.log.apply(null, arguments);
+    bufferedConsole.log.apply(null, arguments);
   }
   static isDebug() {
     return debug;
   }
   static enableDebug() {
     debug = true;
+  }
+  static newBuffer() {
+    initConsole();
+  }
+  static readBuffer() {
+    return logBuffer.getContentsAsString('utf8');
   }
 }
 
