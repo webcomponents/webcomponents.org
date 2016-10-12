370beed95ce1964b58e29a1af7e2c6aff82ef925
Douglas Stockwell <dstockwell@chromium.org>
2016-10-12 13:58:14 +1100

    Support top level components directory and index.html in usercontent (#457)
    * Support top level components directory and index.html in usercontent
    
    Fixes #444
    
    * Attempt to redirect bower_components
    
    * take that, linter!

--- src/user_content.py
+++ src/user_content.py
@@ -4,10 +4,15 @@ from google.appengine.ext import ndb
 import json
 import webapp2
 
-from datamodel import Library, Version, Content
+from datamodel import Library, Version, Content, Status
+
+class RedirectResource(webapp2.RequestHandler):
+  def get(self, owner, repo, tag, path):
+    self.response.headers['cache-control'] = 'max-age=315569000'
+    self.redirect('/%s/%s/%s/%s' % (owner, repo, tag, path), permanent=True)
 
 class GetResource(webapp2.RequestHandler):
-  def get(self, owner, repo, tag, name=None, path=None):
+  def get(self, owner, repo, tag, name, path):
     self.response.headers['Access-Control-Allow-Origin'] = '*'
     self.response.headers['Content-Type'] = 'application/json'
 
@@ -15,8 +20,11 @@ class GetResource(webapp2.RequestHandler):
     repo = repo.lower()
     version_key = ndb.Key(Library, '%s/%s' % (owner, repo), Version, tag)
 
+    if path.endswith('/'):
+      path = path + 'index.html'
+
     analysis = Content.get_by_id('analysis', parent=version_key, read_policy=ndb.EVENTUAL_CONSISTENCY)
-    if analysis is None:
+    if analysis is None or analysis.status != Status.ready:
       self.response.write('could not find analysis for %s in %s/%s' % (tag, owner, repo))
       self.response.set_status(404)
       return
@@ -72,5 +80,6 @@ class GetResource(webapp2.RequestHandler):
 # pylint: disable=invalid-name
 app = webapp2.WSGIApplication([
     webapp2.Route(r'/<owner>/<repo>/<tag>', handler=GetResource),
+    webapp2.Route(r'/<owner>/<repo>/<tag>/<:>/<:(bower_)?components>/<path:.*>', handler=RedirectResource),
     webapp2.Route(r'/<owner>/<repo>/<tag>/<name><path:/.*>', handler=GetResource),
 ], debug=True)
