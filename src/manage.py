80be1b151994ea201da6894ab51c02fd36f8cf6f
Andy Mutton <andymutton@google.com>
2016-10-19 10:45:51 +1100

    use-github-api-for-all-readme-requests

--- src/manage.py
+++ src/manage.py
@@ -588,21 +588,29 @@ class IngestVersion(RequestHandler):
 
   def update_readme(self):
     readme = None
-    for readme_file_name in ['README.md', 'readme.md', 'ReadMe.md', 'Readme.md']:
-      response = urlfetch.fetch(util.content_url(self.owner, self.repo, self.sha, readme_file_name), validate_certificate=True)
-      if response.status_code == 200:
-        readme = response.content
+    readme_api_url = util.github_url('repos', self.owner, self.repo, 'readme') + '?ref=%s' % self.sha
+    readme_api_response = urlfetch.fetch(readme_api_url)
+    if readme_api_response.status_code == 200:
+      readme_url = json.loads(readme_api_response.content)['download_url']
+      readme_response = urlfetch.fetch(readme_url)
+      if readme_response.status_code == 200:
+        readme = readme_response.content
         try:
           Content(parent=self.version_key, id='readme', content=readme,
-                  status=Status.ready, etag=response.headers.get('ETag', None)).put()
+                  status=Status.ready, etag=readme_response.headers.get('ETag', None)).put()
           break
         except db.BadValueError:
           return self.error("Could not store README.md as a utf-8 string")
-      elif response.status_code == 404:
-        logging.info('no readme found at %s', readme_file_name)
-        continue
+      elif readme_response.status_code == 404:
+        logging.info('no readme found at %s', readme_url)
+        readme = None
       else:
-        return self.retry('error fetching readme (%d)' % response.status_code)
+        return self.retry('error fetching readme (%d)' % readme_response.status_code)
+    elif readme_api_response.status_code == 404:
+      logging.info('no readme found at %s', readme_api_url)
+      readme = None
+    else:
+      return self.retry('error fetching readme (%d)' % readme_api_response.status_code)
 
     if readme is not None:
       response = util.github_markdown(readme)
