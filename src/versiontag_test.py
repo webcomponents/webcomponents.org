f4ec6dedcb3398714be152c736d6f710dcbc737a
Douglas Stockwell <dstockwell@chromium.org>
2016-11-07 13:41:41 +1100

    Log whether triggered version ingestions are major/minor/patch
    Fixes #635

--- src/versiontag_test.py
+++ src/versiontag_test.py
@@ -25,3 +25,17 @@ class VersionTagTest(unittest.TestCase):
     self.assertFalse(versiontag.match('2.0.0', '~1'))
     self.assertFalse(versiontag.match('0.1.0', '~1'))
     self.assertFalse(versiontag.match('0.0.1', '~1'))
+
+  def test_categorize(self):
+    self.assertEqual(versiontag.categorize('v1.0.0', []), 'unknown')
+    self.assertEqual(versiontag.categorize('v2.0.0', ['v1.0.0']), 'major')
+    self.assertEqual(versiontag.categorize('v1.1.0', ['v1.0.0']), 'minor')
+    self.assertEqual(versiontag.categorize('v1.1.1', ['v1.0.0']), 'minor')
+    self.assertEqual(versiontag.categorize('v1.0.1', ['v1.0.0']), 'patch')
+    self.assertEqual(versiontag.categorize('1.0.1', ['v1.0.0']), 'patch')
+
+    self.assertEqual(versiontag.categorize('bestversionever', ['v1.0.0']), 'unknown')
+
+    self.assertEqual(versiontag.categorize('v2.0.0', ['v1.0.0', 'v3.0.0']), 'major')
+    self.assertEqual(versiontag.categorize('v2.1.0', ['v1.0.0', 'v3.0.0']), 'major')
+    self.assertEqual(versiontag.categorize('v2.1.1', ['v1.0.0', 'v2.1.0', 'v3.0.0']), 'patch')
