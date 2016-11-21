a3f4221ad3859a5f307ad5d8b5b8aee61b61b049
Sam Li <samli@google.com>
2016-11-22 09:36:59 +1100

    Support paths which don't require app shell (#666)

--- client/sw-precache-config.js
+++ client/sw-precache-config.js
@@ -3,6 +3,12 @@ module.exports = {
     'service-worker-runtime.js',
   ],
   navigateFallback: '/index.html',
+  // Must be kept in sync with client.yaml
+  navigateFallbackWhitelist: [
+    /^\/(index.html)?$/,
+    /^\/(about|introduction|community|assets)(\/.*)?$/,
+    /^\/(author|collections?|elements?|search|preview|preview-integration|publish|publish-collection)(\/.*)?$/,
+  ],
   runtimeCaching: [{
     urlPattern: /DUMMY/,
     handler: 'networkFirst',
