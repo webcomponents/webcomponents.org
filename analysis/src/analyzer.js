c5877929f9b78ae1e048d690443c06539bc21a88
Samuel Li <samli@google.com>
2017-03-06 17:35:06 -0800

    Update to newer version of analyzer

--- analysis/src/analyzer.js
+++ analysis/src/analyzer.js
@@ -2,93 +2,39 @@
 
 const Ana = require('./ana_log');
 
-const PolymerAnalyzer = require('polymer-analyzer').Analyzer;
-const Element = require('polymer-analyzer/lib/model/model').Element;
+const Analyzer = require('polymer-analyzer').Analyzer;
+const Elements = require('polymer-analyzer/lib/elements-format');
+const generateAnalysis = require('polymer-analyzer/lib/generate-analysis').generateAnalysis;
+const Feature = require('polymer-analyzer/lib/model/model');
 const FSUrlLoader = require('polymer-analyzer/lib/url-loader/fs-url-loader').FSUrlLoader;
 const PackageUrlResolver = require('polymer-analyzer/lib/url-loader/package-url-resolver').PackageUrlResolver;
-const path = require('path');
 
-/**
- * @param {Object} obj - The object to strip properties from.
- * @param {Array.<string>} properties - An array of property names to remove.
- * @param {number} depth - The current depth of the recursion.
- */
-function removeProperties(obj, properties, depth) {
-  // Bail after 10 deep so that we don't blow the stack.
-  if (!properties || !obj || depth > 10) return;
-  if (typeof obj === 'object') {
-    for (var prop of properties) {
-      delete obj[prop];
-    }
-    Object.keys(obj).forEach(x => removeProperties(obj[x], properties, depth + 1));
-  } else if (Array.isArray(obj)) {
-    obj.forEach(val => removeProperties(val, properties, depth + 1));
-  }
-}
-
-function remove(obj) {
-  removeProperties(obj, ["scriptElement", "javascriptNode",
-      "observerNode", "astNode", "sourceRange", "domModule"], 0);
-}
-
-/**
- * Service for running Polymer Analyzer on the local machine.
- */
-class Analyzer {
-
-  /**
-   * Runs Polymer Analyzer against each main html file in the given list.
-   * Merges Element and Behavior key/values from each main html into one set each.
-   * Output looks like {elementsByTagName:{}, behaviorsByName:{}}.
-   * @param {Array.<string>} mainHtmlPaths - paths to mainHtml files
-   * @return {Promise.<Array.<Object>>} a promise, returning the element and behavior data.
-   */
-  analyze(mainHtmlPaths) {
-    /*
-     * Run Analyzis on given paths and extract relevant data. Failure is fine, we just ignore it.
-     */
+class AnalyzerRunner {
+  analyze(inputs) {
     return new Promise(resolve => {
-      Ana.log("analyzer/analyze", mainHtmlPaths);
-      var data = {
-        elementsByTagName: {},
-        behaviorsByName: {}
-      };
+      Ana.log('analyzer/analyze', inputs);
 
-      Promise.all(
-        mainHtmlPaths.map(mainHtmlPath => {
-          var analyzer = new PolymerAnalyzer({
-            urlLoader: new FSUrlLoader("/"),
-            urlResolver: new PackageUrlResolver(),
-          });
-
-          var dirName = path.dirname(mainHtmlPath);
-          var skipItem = item => path.relative(dirName, "/" + item.sourceRange.file).includes("..");
-
-          return analyzer.analyze(mainHtmlPath).then(document => {
-            var elements = document.getByKind('element');
-            elements.forEach(element => {
-              if (skipItem(element)) return;
-
-              data.elementsByTagName[element.tagName] = element;
-              remove(data.elementsByTagName[element.tagName])
-            });
-            var behaviors = document.getByKind('behavior');
-            behaviors.forEach(behavior => {
-              if (skipItem(behavior)) return;
-
-              data.behaviorsByName[behavior.className] = behavior;
-              remove(data.behaviorsByName[behavior.className])
-            });
-          }).catch(error => {
-            Ana.fail("analyzer/analyze", mainHtmlPath, error);
-          });
-        })
-      ).then(function() {
-        Ana.success("analyzer/analyze", mainHtmlPaths);
-        resolve(data);
+      const analyzer = new Analyzer({
+        urlLoader: new FSUrlLoader('/'),
+        urlResolver: new PackageUrlResolver(),
       });
+
+      const isInTests = /(\b|\/|\\)(test)(\/|\\)/;
+      const isNotTest = feature =>
+          feature.sourceRange != null && !isInTests.test(feature.sourceRange.file);
+
+      if (inputs == null || inputs.length === 0) {
+        analyzer.analyzePackage().then(function(_package) {
+          resolve(generateAnalysis(_package, '', isNotTest));
+        });
+      } else {
+        Promise.all(inputs.map((i) => analyzer.analyze(i))).then(function(documents) {
+          resolve(generateAnalysis(documents, '', isNotTest));
+        });
+      }
+
     });
   }
 }
 
-module.exports = Analyzer;
+module.exports = AnalyzerRunner;
