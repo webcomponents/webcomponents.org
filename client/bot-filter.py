e9e065aa4b8bcd4af3465c8461918f00a0c9a920
Sam li <samli@google.com>
2017-04-03 16:21:48 -0700

    Lint

--- client/bot-filter.py
+++ client/bot-filter.py
@@ -12,7 +12,7 @@ class FilterUserAgent(webapp2.RequestHandler):
     if filter:
       try:
         # Set deadline for fetch to 10 seconds
-        urlfetch.set_default_fetch_deadline(20)
+        urlfetch.set_default_fetch_deadline(10)
         result = urlfetch.fetch('https://bot-render.appspot.com/?url=%s' % self.request.url)
         if result.status_code == 200:
           self.response.write(result.content)
@@ -20,8 +20,7 @@ class FilterUserAgent(webapp2.RequestHandler):
           self.response.status_code = result.status_code
         return
       except urlfetch.Error:
-          logging.exception('Caught exception fetching url')
-
+        logging.exception('Caught exception fetching url')
 
     file = open('index.html', 'r')
     self.response.out.write(file.read())
