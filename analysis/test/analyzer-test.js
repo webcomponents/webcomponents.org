86dd2c189bcd41c673a56a05ba95d6d5cd8dd84f
Sam li <samli@google.com>
2017-05-08 12:19:49 -0700

    Update to latest analyzer and iron-doc-viewer

--- analysis/test/analyzer-test.js
+++ analysis/test/analyzer-test.js
@@ -20,14 +20,15 @@ describe('AnalyzerRunner', function() {
       expect(result).to.exist;
       expect(JSON.stringify(result)).to.exist;
       expect(result.elements).to.have.lengthOf(1);
-      expect(result.elements[0]).to.have.property('classname', 'ShopImage');
+      expect(result.elements[0]).to.have.property('name', 'ShopImage');
     });
   });
 
   it('ignores unknown files', function(done) {
     var analyzer = new AnalyzerRunner();
-    analyzer.analyze(path.resolve(__dirname, 'resources'), ['does-not-exist.not-found']).catch(function() {
-      // Successfully rejected promise.
+    analyzer.analyze(path.resolve(__dirname, 'resources'), ['does-not-exist.not-found']).then(function(result) {
+      // Doesn't throw anymore
+      expect(result).to.exist;
       done();
     });
   });
@@ -37,13 +38,25 @@ describe('AnalyzerRunner', function() {
     return analyzer.analyze(path.resolve(__dirname, 'resources/meta-repo'), ['import-more.html']).then(function(result) {
       expect(result).to.exist;
       expect(JSON.stringify(result)).to.exist;
-      expect(result.elements).to.have.lengthOf(2);
+      expect(result.elements).to.have.lengthOf(3);
 
       expect(result).to.have.property('metadata');
       expect(result.metadata).to.have.property('polymer');
       expect(result.metadata.polymer).to.have.property('behaviors');
       expect(result.metadata.polymer.behaviors).to.have.lengthOf(1);
       expect(result.metadata.polymer.behaviors[0]).to.have.property('name', 'Polymer.MyBehavior');
+    });
+  });
+
+  it('includes demos with descriptions', function() {
+    var analyzer = new AnalyzerRunner();
+    return analyzer.analyze(path.resolve(__dirname, 'resources'), ['element-with-demo.html']).then(function(result) {
+      expect(result).to.exist;
+      expect(JSON.stringify(result)).to.exist;
+      expect(result.elements).to.have.lengthOf(1);
+      expect(result.elements[0].demos).to.have.lengthOf(1);
+      expect(result.elements[0].demos[0]).to.have.property('url', 'demo.html');
+      expect(result.elements[0].demos[0]).to.have.property('description', 'description');
     });
   });
 });
