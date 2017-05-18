8fe441af4aa0f176d31d59b2a016a81b7f816691
Sam Li <samli@google.com>
2017-05-18 10:42:40 -0700

    Fix inherited properties in analysis (#973)

--- analysis/src/analyzer.js
+++ analysis/src/analyzer.js
@@ -2,33 +2,33 @@
 
 const Ana = require('./ana_log');
 
-const Analyzer = require('polymer-analyzer').Analyzer;
-const Analysis = require('polymer-analyzer/lib/analysis-format');
-const generateAnalysis = require('polymer-analyzer/lib/generate-analysis').generateAnalysis;
-const Feature = require('polymer-analyzer/lib/model/model');
+const {Analyzer, generateAnalysis} = require('polymer-analyzer');
 const FSUrlLoader = require('polymer-analyzer/lib/url-loader/fs-url-loader').FSUrlLoader;
 const PackageUrlResolver = require('polymer-analyzer/lib/url-loader/package-url-resolver').PackageUrlResolver;
+const path = require('path');
 
 class AnalyzerRunner {
   analyze(root, inputs) {
     return new Promise((resolve, reject) => {
       Ana.log('analyzer/analyze', inputs);
 
+      // Move up a directory so analyzer will look at all dependencies properly.
+      var analyzerRoot = path.dirname(root);
+      var paths = inputs.map(x => path.join(path.basename(root), x));
+
       const analyzer = new Analyzer({
-        urlLoader: new FSUrlLoader(root),
+        urlLoader: new FSUrlLoader(analyzerRoot),
         urlResolver: new PackageUrlResolver(),
       });
 
-      const isInTests = /(\b|\/|\\)(test)(\/|\\)/;
-      const isNotTest = feature =>
-          feature.sourceRange != null && !isInTests.test(feature.sourceRange.file);
+      const isInPackage = feature => feature.sourceRange != null && feature.sourceRange.file.startsWith(path.basename(root));
 
       if (inputs == null || inputs.length === 0) {
         resolve({});
         // TODO: fall back to package analysis
       } else {
-        analyzer.analyze(inputs).then(function(analysis) {
-          resolve(generateAnalysis(analysis, root));
+        analyzer.analyze(paths).then(function(analysis) {
+          resolve(generateAnalysis(analysis, root, isInPackage));
         }).catch(function(error) {
           Ana.fail('analyzer/analyze', inputs, error);
           reject({retry: true, error: error});
