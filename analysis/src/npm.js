f5da889e55b85442a5e0f40c7495c0b1fe3f214e
Sam li <samli@google.com>
2017-06-15 12:33:55 -0700

    findDependencies() implemented

--- analysis/src/npm.js
+++ analysis/src/npm.js
@@ -24,7 +24,6 @@ class NPM {
     if (!fs.existsSync('installed')){
       fs.mkdirSync('installed');
     }
-    fs.writeFileSync('installed/package.json', '{}');
     npm.load();
   }
 
@@ -65,6 +64,7 @@ class NPM {
   prune() {
     return new Promise((resolve, reject) => {
       Ana.log("npm/prune");
+      fs.writeFileSync('installed/package.json', '{}');
       childProcess.exec("rm -rf installed/node_modules", function(err) {
         if (err) {
           Ana.fail("npm/prune");
@@ -77,17 +77,22 @@ class NPM {
     });
   }
 
-  install(scope, packageName, version) {
+  _packageToInstall(scope, packageName, version) {
     const scopePath = scope == '@@npm' ? '' : scope + '/';
-    const packageToInstall = scopePath + packageName + '@' + version;
+    return scopePath + packageName + '@' + version;
+  }
+
+  install(scope, packageName, version) {
+    const packageToInstall = this._packageToInstall(scope, packageName, version);
     Ana.log('npm/install', packageToInstall);
     return new Promise((resolve, reject) => {
       let opts = {
         cwd: path.resolve('installed')
       };
-      this._exec(['install', '--loglevel=silent', '--no-save', '--only=prod', packageToInstall], opts).then((code, stdout, stderr) => {
+      this._exec(['install', '--loglevel=silent', '--save', '--only=prod', packageToInstall], opts).then((code, stdout, stderr) => {
         if (code != 0) {
-          console.log(code);
+          // TODO(samli): there's an error!
+          // ETARGET seems to have a code of 1
           reject();
         }
         resolve();
@@ -99,6 +104,50 @@ class NPM {
           retry = false;
         }
         reject({retry: retry, error: error});
+      });
+    });
+  }
+
+  _getDependencies(obj) {
+    var result = [];
+    if (!obj.dependencies)
+      return result;
+    Object.keys(obj.dependencies).forEach(key => {
+      result.push(key + '@' + obj.dependencies[key].version);
+      result = result.concat(this._getDependencies(obj.dependencies[key]));
+    });
+    return result;
+  }
+
+  findDependencies(scope, packageName, version) {
+    const scopePath = scope == '@@npm' ? '' : scope + '/';
+    const packageString = this._packageToInstall(scope, packageName, version);
+    Ana.log('npm/findDependencies', packageString);
+    return new Promise((resolve, reject) => {
+      let opts = {
+        cwd: path.resolve('installed')
+      };
+      this._exec(['list', '--json', '--loglevel', 'silent', '--prod'], opts).then((result) => {
+        const [code, stdout, stderr] = result;
+        // Don't reject() since NPM can return non zero codes for warnings like
+        // unmet dependencies, extraneous etc.
+        if (code != 0) {
+          Ana.log('npm/findDependencies', packageString, 'returned code:', code);
+        }
+
+        // For some reason this breaks on really large sets of dependencies.
+        var info;
+        try {
+          info = JSON.parse(stdout || {});
+        } catch(e) {
+          reject({retry: false, error: error});
+          return;
+        }
+        const deps = this._getDependencies(info.dependencies[scopePath + packageName]);
+        resolve(deps);
+      }).catch(error => {
+        Ana.fail('npm/findDependencies', packageString);
+        reject({retry: false, error: error});
       });
     });
   }
