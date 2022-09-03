f8ff5fe4ea2df833ed0d632683d33897a1578be9
Justin Fagnani <justinfagnani@google.com>
2022-09-02 17:16:23 -0700

    Fix resolveReference and add tests

--- packages/custom-elements-manifest-tools/src/lib/resolve-reference.ts
+++ packages/custom-elements-manifest-tools/src/lib/resolve-reference.ts
@@ -35,34 +35,46 @@ export const resolveReference = (
     console.warn("Can't resolve cross-package reference", ref);
     return undefined;
   }
-  // Local reference
-  const mod =
-    ref.module === undefined ? localModule : getModule(pkg, ref.module);
-  if (mod === undefined) {
-    // Module not found
-    const modules = pkg.modules.map((m) => m.path);
-    console.warn(
-      "Can't find module",
-      packageName,
-      packageVersion,
-      ref.module,
-      modules
-    );
-    return undefined;
-  }
-  if (mod.declarations) {
-    for (const d of mod.declarations) {
-      if (d.name === ref.name) {
-        return d;
-      }
+  if (ref.module === undefined) {
+    // Local reference. Local references refer to declarations in the local
+    // module scope.
+    return localModule.declarations?.find((d) => d.name === ref.name);
+  } else {
+    // Cross-module reference. A reference to a different module references
+    // an export
+    const mod = getModule(pkg, ref.module);
+    if (mod === undefined) {
+      // Module not found
+      const modules = pkg.modules.map((m) => m.path);
+      console.warn(
+        "Can't find module",
+        packageName,
+        packageVersion,
+        ref.module,
+        modules
+      );
+      return undefined;
     }
+    const exprt = mod.exports?.find((e) => e.name === ref.name);
+    if (exprt === undefined) {
+      console.warn(
+        "Can't find export",
+        packageName,
+        packageVersion,
+        ref.module,
+        ref.name
+      );
+      return undefined;
+    }
+    const declaration = exprt.declaration;
+    // Recurse to find the declaration with the new module as the local module.
+    // There are two cases:
+    //  - declaration is local to the new module (it's module property is
+    //    undefined), in which case the recursion call is to look up a local
+    //    reference to a declaration within the new module.
+    //  - declaration is a reference to another module, (module is defined) in
+    //    which case the recursion call will look for an export of the new
+    //    module, then recurse again to find the declaration.
+    return resolveReference(pkg, mod, declaration, packageName, packageVersion);
   }
-  console.warn(
-    "Can't find declaration",
-    packageName,
-    packageVersion,
-    ref.module,
-    ref.name
-  );
-  return undefined;
 };
