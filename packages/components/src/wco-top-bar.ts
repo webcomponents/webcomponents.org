3da0b75d25e9c6665df8b22d6eb80656907538ef
Alexander Marks <aomarks@gmail.com>
2022-09-23 13:12:01 -0700

    Eleventy site skeleton (#1333)
    - Adds a `content` package with a "hello world" eleventy site
    - Adds a `components` package with a basic element
    - Adds a top-level `dev` command
    - A few misc small cleanupy things
    
    Will add a server in a followup, and then get auto-deploy working

---
+++ packages/components/src/wco-top-bar.ts
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {html, css, LitElement} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('wco-top-bar')
+export class WCOTopBar extends LitElement {
+  static styles = css`
+    :host {
+      position: sticky;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 50px;
+      padding: 10px 20px;
+      display: flex;
+      align-items: center;
+      background: #eee;
+      border-bottom: 1px solid #ccc;
+      font-family: sans-serif;
+    }
+  `;
+
+  render() {
+    return html` webcomponents.org `;
+  }
+}
