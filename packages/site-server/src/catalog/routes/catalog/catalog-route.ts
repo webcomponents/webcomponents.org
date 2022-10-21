a644796a0bd96b18a5b01ca56482c7678432b59e
Justin Fagnani <justinfagnani@google.com>
2022-10-20 20:22:31 -0700

    WIP

--- packages/site-server/src/catalog/routes/catalog/catalog-route.ts
+++ packages/site-server/src/catalog/routes/catalog/catalog-route.ts
@@ -4,10 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import Router from '@koa/router';
-import {renderPage} from '../../../templates/base.js';
 import {DefaultContext, DefaultState, ParameterizedContext} from 'koa';
+import {html} from 'lit';
 import {Readable} from 'stream';
+import Router from '@koa/router';
+import {render} from '@lit-labs/ssr/lib/render-with-global-dom-shim.js';
+
+import '@webcomponents/internal-site-client/lib/entrypoints/catalog.js';
+import {renderPage} from '../../../templates/base.js';
 
 export const handleCatalogRoute = async (
   context: ParameterizedContext<
@@ -16,17 +20,17 @@ export const handleCatalogRoute = async (
     unknown
   >
 ) => {
-  context.body = Readable.from(renderPage({
-    title: `Web Components Catalog`,
-    scripts: [
-      '/js/catalog.js'
-    ],
-    content: `
-    <wco-top-bar></wco-top-bar>
-    <h1>Catalog</h1>
-    <wco-catalog-search></wco-catalog-search>
-  `,
-  }));
+  context.body = Readable.from(
+    renderPage({
+      title: `Web Components Catalog`,
+      scripts: ['/js/hydrate.js', '/js/catalog.js'],
+      content: render(html`
+        <wco-top-bar></wco-top-bar>
+        <h1>Catalog</h1>
+        <wco-catalog-search></wco-catalog-search>
+      `),
+    })
+  );
   context.type = 'html';
   context.status = 200;
 };
