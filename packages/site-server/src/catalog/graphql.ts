e04fb55b2c228fcdb53fb8d07fcb82e839e156dd
Justin Fagnani <justinfagnani@google.com>
2022-12-06 18:42:08 -0800

    Authenticate service-to-service requests

--- packages/site-server/src/catalog/graphql.ts
+++ packages/site-server/src/catalog/graphql.ts
@@ -4,12 +4,39 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {ApolloClient, InMemoryCache} from '@apollo/client/core/index.js';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client/core/index.js';
+import {GoogleAuth} from 'google-auth-library';
 
 const CATALOG_GRAPHQL_URL =
   process.env['CATALOG_GRAPHQL_URL'] || `http://localhost:6451/graphql`;
 
-export const client = new ApolloClient({
+// The GoogleAuth class will use Application Default Credientials,
+// which are automatically provided to GCP services with attached
+// service accounts.
+// See:
+// - https://github.com/googleapis/google-auth-library-nodejs#application-default-credentials
+// - https://cloud.google.com/docs/authentication/provide-credentials-adc#attached-sa
+const auth = new GoogleAuth({
+  scopes: 'https://www.googleapis.com/auth/cloud-platform',
+});
+const authClient = await auth.getClient();
+
+const link = new HttpLink({
   uri: CATALOG_GRAPHQL_URL,
+  async fetch(
+    input: RequestInfo | URL,
+    init?: RequestInit | undefined
+  ): Promise<Response> {
+    const headers = await authClient.getRequestHeaders();
+    return fetch(input, {...(init ?? {}), headers});
+  },
+});
+
+export const client = new ApolloClient({
+  link,
   cache: new InMemoryCache(),
 });
