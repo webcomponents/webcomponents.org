5d9129641696559616d38c34f3e139b8446af449
Alexander Marks <aomarks@google.com>
2023-01-11 17:23:56 -0800

    Move nav HTML generation into wco-nav-page component

--- packages/site-client/src/components/wco-nav-page.ts
+++ packages/site-client/src/components/wco-nav-page.ts
@@ -4,10 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {html, css} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {html, css, TemplateResult} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {classMap} from 'lit/directives/class-map.js';
 import {WCOPage} from './wco-page.js';
 import './wco-top-bar.js';
+
+export interface NavEntry {
+  key: string;
+  url: string;
+  children?: NavEntry[];
+}
 
 /**
  * A page with left and right side navigation areas.
@@ -36,15 +43,44 @@ export class WCONavPage extends WCOPage {
         align-items: center;
         border: solid 1px red;
       }
+
+      nav li.active {
+        font-weight: bold;
+      }
     `,
   ];
 
+  @property({attribute: false})
+  navEntries?: NavEntry[];
+
   protected override renderMain() {
     return html`
-      <nav id="main-outline"><slot name="outline"></slot></nav>
+      <nav id="main-outline">${this._renderNav(this.navEntries)}</nav>
       <article>${this.renderContent()}</article>
       <nav id="right-nav">[Page ToC]</nav>
     `;
+  }
+
+  private _renderNav(
+    entries: NavEntry[] | undefined
+  ): TemplateResult | undefined {
+    if (entries === undefined || entries.length === 0) {
+      return;
+    }
+    return html`<ul>
+      ${entries.map(
+        (entry) => html`<li
+          class="${classMap({
+            active: entry.url === globalThis.location.pathname,
+          })}"
+        >
+          ${entry.url
+            ? html`<a href="${entry.url}">${entry.key}</a>`
+            : entry.key}
+          ${this._renderNav(entry.children)}
+        </li>`
+      )}
+    </ul>`;
   }
 }
 
--- packages/site-content/site/_includes/layouts/docs.11ty.cjs
+++ packages/site-content/site/_includes/layouts/docs.11ty.cjs
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const {navToHTML} = require('./nav.cjs');
-
 module.exports = {
   async render(data) {
     const {renderPage, html, unsafeHTML} = await import(
@@ -17,21 +15,18 @@ module.exports = {
 
     // Set location because wco-nav-bar reads pathname from it. URL isn't
     // exactly a Location, but it's close enough for read-only uses
-    globalThis.location = new URL('http://localhost:5450/docs/');
 
-    const navigationEntries = this.eleventyNavigation(data.collections.all);
-
-    const navigationHTML = navToHTML(
-      navigationEntries,
-      {slot: 'outline'},
-      data.page
+    globalThis.location = new URL(
+      `http://localhost:5450${data.page.url || '/'}`
     );
+
+    const navEntries = this.eleventyNavigation(data.collections.all);
 
     return [
       ...renderPage({
         ...data,
-        content: html`<wco-docs-page>
-          ${unsafeHTML(navigationHTML)} ${unsafeHTML(data.content)}
+        content: html`<wco-docs-page .navEntries=${navEntries}>
+          ${unsafeHTML(data.content)}
         </wco-docs-page>`,
       }),
     ].join('');
