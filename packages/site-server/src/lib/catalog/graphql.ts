9847471d7e74ba44e0938044ff008c13234a59d2
Alexander Marks <aomarks@google.com>
2023-01-12 15:25:13 -0800

    Authenticate site -> catalog requests

--- packages/site-server/src/lib/catalog/graphql.ts
+++ packages/site-server/src/lib/catalog/graphql.ts
@@ -4,12 +4,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {ApolloClient, InMemoryCache} from '@apollo/client/core/index.js';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client/core/index.js';
+import {GoogleAuth} from 'google-auth-library';
 
-const CATALOG_GRAPHQL_URL =
+let CATALOG_GRAPHQL_URL =
   process.env['CATALOG_GRAPHQL_URL'] || `http://localhost:6451/graphql`;
 
+if (!CATALOG_GRAPHQL_URL.endsWith('/')) {
+  CATALOG_GRAPHQL_URL = CATALOG_GRAPHQL_URL + '/';
+}
+
+const auth = new GoogleAuth();
+
+const link = new HttpLink({
+  uri: CATALOG_GRAPHQL_URL + 'graphql',
+  async fetch(
+    input: RequestInfo | URL,
+    init?: RequestInit | undefined
+  ): Promise<Response> {
+    const authClient = await auth.getIdTokenClient(CATALOG_GRAPHQL_URL);
+    const authHeaders = await authClient.getRequestHeaders();
+    const headers = {
+      ...(init?.headers ?? {}),
+      ...authHeaders,
+    };
+    console.log('audience', CATALOG_GRAPHQL_URL);
+    console.log('input', input);
+    console.log('init', init);
+    console.log('request headers', headers);
+
+    return fetch(input, {
+      ...(init ?? {}),
+      headers,
+    });
+  },
+});
+
 export const client = new ApolloClient({
-  uri: CATALOG_GRAPHQL_URL,
+  link,
   cache: new InMemoryCache(),
 });
