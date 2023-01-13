2006c47f45a87a460f25913ba44e8e955f9e01b0
Alexander Marks <aomarks@google.com>
2023-01-13 14:11:40 -0800

    Move "hydrate" modules to "boot"

--- packages/site-client/src/entrypoints/docs-hydrate.ts
+++ packages/site-client/src/entrypoints/docs-hydrate.ts
@@ -4,12 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {NavEntry} from '../shared/wco-nav-page.js';
-
-const data = (globalThis as unknown as {__ssrData: [NavEntry[]]}).__ssrData;
-
-const navEntries = data[0];
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const el = document.querySelector('wco-docs-page')!;
-el.navEntries = navEntries;
-el.removeAttribute('defer-hydration');
+export * from '../pages/docs/boot.js';
--- packages/site-client/src/entrypoints/element-hydrate.ts
+++ packages/site-client/src/entrypoints/element-hydrate.ts
@@ -4,15 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {hydrate} from 'lit/experimental-hydrate.js';
-import {renderElementPage} from './element.js';
-
-const data = (
-  globalThis as unknown as {__ssrData: Parameters<typeof renderElementPage>}
-).__ssrData;
-
-// We need to hydrate the whole page to remove any defer-hydration attributes.
-// We could also remove the attribute manually, or not use deferhydration, but
-// instead manually assign the data into the <wco-element-page> element, and
-// time imports so that automatic element hydration happend after.
-hydrate(renderElementPage(...data), document.body);
+export * from '../pages/element/boot.js';
