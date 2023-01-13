077130b2318de4f3354e73564c6e6027d2393218
Justin Fagnani <justinfagnani@google.com>
2023-01-13 13:57:03 -0800

    Add search endpoint to site-server

---
+++ packages/site-server/src/lib/catalog/routes/catalog/search-route.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {DefaultContext, DefaultState, ParameterizedContext} from 'koa';
+import Router from '@koa/router';
+import {client} from '../../graphql.js';
+import {gql} from '@apollo/client/core/index.js';
+
+import '@webcomponents/internal-site-client/lib/entrypoints/catalog.js';
+
+const elementsQuery = gql`
+  query Elements($query: String) {
+    elements(query: $query, limit: 16) {
+      tagName
+      package
+      version
+      className
+    }
+  }
+`;
+
+export const handleCatalogSearchRoute = async (
+  context: ParameterizedContext<
+    DefaultState,
+    DefaultContext & Router.RouterParamContext<DefaultState, DefaultContext>,
+    unknown
+  >
+) => {
+  const searchText = context.query['query'];
+  context.type = 'json';
+
+  if (typeof searchText !== 'string') {
+    context.status = 400;
+    context.body = {
+      message: 'Query parmeter `query` must be a string',
+    };
+    return;
+  }
+
+  const result = await client.query({
+    query: elementsQuery,
+    variables: {query: searchText},
+  });
+
+  if (result.error) {
+    context.status = 500;
+    context.body = {
+      message: result.error,
+    };
+    return;
+  }
+
+  context.status = 200;
+  context.body = {
+    elements: result.data?.elements,
+  };
+};
