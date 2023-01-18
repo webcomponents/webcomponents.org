87974a6151dc3976c9b299a6a4bb86d206b556e8
Justin Fagnani <justinfagnani@google.com>
2023-01-17 19:01:42 -0800

    Add rudimentary catalog import page

--- packages/site-server/src/lib/catalog/router.ts
+++ packages/site-server/src/lib/catalog/router.ts
@@ -5,14 +5,23 @@
  */
 
 import Router from '@koa/router';
-import {handleCatalogRoute} from './routes/catalog-route.js';
-import {handleCatalogSearchRoute} from './routes/search-route.js';
-import {handleElementRoute} from './routes/element-route.js';
+import {handleCatalogRoute} from './routes/catalog-page.js';
+import {handleCatalogImportRoute} from './routes/import-page.js';
+import {handleCatalogImportApiRoute} from './routes/import-api.js';
+import {handleElementRoute} from './routes/element-page.js';
+import {handleCatalogSearchRoute} from './routes/search-api.js';
+import bodyParser from 'koa-bodyparser';
 
 export const catalogRouter = new Router();
+
+// Needed for /import
+catalogRouter.use(bodyParser());
 
 catalogRouter.get('/', handleCatalogRoute);
 
 catalogRouter.get('/search', handleCatalogSearchRoute);
+
+catalogRouter.get('/import', handleCatalogImportRoute);
+catalogRouter.post('/import', handleCatalogImportApiRoute);
 
 catalogRouter.get('/element/:path+', handleElementRoute);
