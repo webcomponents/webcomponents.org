a8d80b74b7b8c0ef90b72d0d91eb0de893fab746
Justin Fagnani <justinfagnani@google.com>
2023-01-18 11:00:14 -0800

    Add a fallback renderer that doesn't render a shadow root

--- packages/site-server/src/lib/catalog/routes/catalog-page.ts
+++ packages/site-server/src/lib/catalog/routes/catalog-page.ts
@@ -40,17 +40,75 @@ const excludeElements = (
       tagName: string,
       attributes: Map<string, string>
     ) {
+      console.log('matchesClass', tagName, !excludedTagNames.includes(tagName));
       return excludedTagNames.includes(tagName)
         ? false
         : super.matchesClass(ceClass, tagName, attributes);
     }
-
-    // This tells SSR to not render a shadow root.
-    // renderShadow _can_ be undefined in lit-html's render-value.ts
-    // We should make it optional in ElementRenderer.
-    renderShadow = undefined as unknown as ElementRenderer['renderShadow'];
   };
 };
+
+const replacements = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  // Note &apos; was not defined in the HTML4 spec, and is not supported by very
+  // old browsers like IE8, so a codepoint entity is used instead.
+  "'": '&#39;',
+};
+
+/**
+ * Replaces characters which have special meaning in HTML (&<>"') with escaped
+ * HTML entities ("&amp;", "&lt;", etc.).
+ */
+const escapeHtml = (str: string) =>
+  str.replace(
+    /[&<>"']/g,
+    (char) => replacements[char as keyof typeof replacements]
+  );
+
+// The built-in FallbackRenderer incorrectly causes a
+// shadow root to be rendered, which breaks hydration
+class FallbackRenderer extends ElementRenderer {
+  static matchesClass(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _ceClass: typeof HTMLElement,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _tagName: string,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _attributes: Map<string, string>
+  ) {
+    return true;
+  }
+  private readonly _attributes: {[name: string]: string} = {};
+
+  override setAttribute(name: string, value: string) {
+    this._attributes[name] = value;
+  }
+
+  override *renderAttributes() {
+    for (const [name, value] of Object.entries(this._attributes)) {
+      if (value === '' || value === undefined || value === null) {
+        yield ` ${name}`;
+      } else {
+        yield ` ${name}="${escapeHtml(value)}"`;
+      }
+    }
+  }
+
+  connectedCallback() {
+    // do nothing
+  }
+  attributeChangedCallback() {
+    // do nothing
+  }
+  *renderLight() {
+    // do nothing
+  }
+
+  declare renderShadow: ElementRenderer['renderShadow'];
+}
 
 export const handleCatalogRoute = async (
   context: ParameterizedContext<
@@ -73,6 +131,7 @@ export const handleCatalogRoute = async (
       {
         elementRenderers: [
           excludeElements(LitElementRenderer, ['md-outlined-text-field']),
+          FallbackRenderer,
         ],
       }
     )
